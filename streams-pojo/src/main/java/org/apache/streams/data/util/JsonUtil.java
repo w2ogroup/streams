0b5304d7bd0049b421757c1c5331ef5a228aae38
mfranklin <mfranklin@apache.org>
2013-09-21 22:41:29 -0400

    Added object mapper util field

--- streams-pojo/src/main/java/org/apache/streams/data/util/JsonUtil.java
+++ streams-pojo/src/main/java/org/apache/streams/data/util/JsonUtil.java
@@ -37,4 +37,22 @@ public class JsonUtil {
             throw new RuntimeException("IO exception while writing JSON", e);
         }
     }
+
+    public static <T> T jsonToObject(String json, Class<T> clazz) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(json, clazz);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not map to object");
+        }
+    }
+
+    public static <T> String objectToJson(T object) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(object);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not map to object");
+        }
+    }
 }
