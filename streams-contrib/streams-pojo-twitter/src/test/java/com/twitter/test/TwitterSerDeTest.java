a66e9aa376b7fc0cc1bf03af767c41e6bbc39399
Melissa DeLaet [W2O Digital] <mdelaet@w2odigital.com>
2013-09-27 10:16:15 -0500

    Place holder files for twitter jsonschema, configuration and test

---
+++ streams-contrib/streams-pojo-twitter/src/test/java/com/twitter/test/TwitterSerDeTest.java
@@ -0,0 +1,11 @@
+package com.twitter.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mdelaet
+ * Date: 9/27/13
+ * Time: 10:13 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TwitterSerDeTest {
+}
